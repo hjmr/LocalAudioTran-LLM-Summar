ed298d0b33a9f029d4d263ec0c0cb7f5aa873dec
hjmr <2620990+hjmr@users.noreply.github.com>
2025-05-30 10:02:44 +0900

    remove build option from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml up -d --build
+docker compose -f docker-compose.yml up -d
 docker exec -it ollama ollama create my-phi3.5  -f /modelfiles/my-phi3.5/Modelfile
 docker exec -it ollama ollama create my-phi4-mini  -f /modelfiles/my-phi4-mini/Modelfile
 docker exec -it ollama ollama pull phi4-mini
