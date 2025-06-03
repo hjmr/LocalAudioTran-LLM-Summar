bed84d8a06772716339cab19cd22af30849a9eca
hjmr <2620990+hjmr@users.noreply.github.com>
2025-06-03 09:09:23 +0900

    set no-gui to default.

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker compose -f docker-compose.yml up -d
+docker compose -f docker-compose-no-gpu.yml up -d
 sleep 3
 docker exec -it ollama ollama pull phi4-mini
-docker exec -it ollama ollama pull mistral-small
 docker exec -it ollama ollama pull gemma3
-docker exec -it ollama ollama pull qwen3
+docker exec -it ollama ollama pull qwen3:4b
 # docker exec -it ollama ollama create my-phi3.5  -f modelfiles/my-phi3.5/Modelfile
 # docker exec -it ollama ollama create my-phi4-mini  -f modelfiles/my-phi4-mini/Modelfile
